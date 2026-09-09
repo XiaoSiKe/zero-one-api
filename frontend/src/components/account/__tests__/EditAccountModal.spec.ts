5b163800f63cf2f3fa5ed211706288e96e403c1b
XiaoSiKe <293472279+XiaoSiKe@users.noreply.github.com>
2026-09-09 19:35:10 +0800

    test: remove obsolete account fixture

--- frontend/src/components/account/__tests__/EditAccountModal.spec.ts
+++ frontend/src/components/account/__tests__/EditAccountModal.spec.ts
@@ -290,18 +290,6 @@ function buildOpenAISetupTokenAccount() {
   } as any
 }
 
-function buildOpenAIOAuthParentAccount() {
-  return {
-    ...buildAccount(),
-    id: 7,
-    name: 'OpenAI OAuth Parent',
-    type: 'oauth',
-    parent_account_id: null,
-    credentials: { access_token: 'oauth-token' },
-    extra: {}
-  } as any
-}
-
 function mountModal(account = buildAccount(), renderGroupSelector = false) {
   return mount(EditAccountModal, {
     props: {
